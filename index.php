edfe9c60bab96a5ffc8b165c53fad249c8fa18e6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-03-06 00:49:03 +0900

    Doc: index.php | PHPDoc

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * unit-form:/index.php
+/** op-unit-form:/index.php
  *
  * @created   2017-01-25
  * @version   1.0
- * @package   unit-form
+ * @package   op-unit-form
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
