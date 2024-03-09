42649e5ebe62529ae3d02eea8e062e1163f150b1
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-03-10 02:06:25 +0900

    Add: testcase > action.php | declare

--- testcase/action.php
+++ testcase/action.php
@@ -7,6 +7,12 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
 /* @var $app    \OP\UNIT\App  */
 
 //	...
